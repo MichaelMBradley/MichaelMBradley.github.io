411bc8f9a5fe6a8e64e798eba5d1f4e518fb3267
Michael Bradley <michaelmbrad@gmail.com>
2024-08-06 23:16:35 -0400

    Optimize build script

--- build.sh
+++ build.sh
@@ -6,4 +6,18 @@ assets_dir="assets"
 mkdir -p "$build_dir"
 minify -o "$build_dir" index.html style.css
 cp -r "$assets_dir" "$build_dir"/
-./fmt.sh "$build_dir"/"$assets_dir"
+
+for file in "$assets_dir"/*; do
+  file_basename="$(basename "$file")"
+  file_name="${file_basename%%.*}"
+  file_extension="${file_basename#*.}"
+  printf "%s ->" "$file_basename"
+  for format in png jpeg webp jxl avif heic; do
+    if [ "$format" = "$file_extension" ]; then
+      continue
+    fi
+    printf " %s" "$format"
+    magick "$file" "${build_dir}/${assets_dir}/${file_name}.${format}"
+  done
+  echo
+done
